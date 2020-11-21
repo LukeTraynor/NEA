e5f9bbb5500b43e60716f7f46a3cd5b4baaf9c97
LukeTraynor <19008512@woodhouse.ac.uk>
2020-11-21 10:19:32 +0000

    GHI-5-Save-and-close-buttons-working-to-disable-text

--- site/public/js/profile.js
+++ site/public/js/profile.js
@@ -1,30 +1,32 @@
 
-$(document).ready(function(){
+ $(document).ready(function() {
+    $('#save_close_button').hide();
+ });
+
+ function toggle_readonly(state){
+    document.getElementById("exampleInputEmail").disabled = state;
+    document.getElementById("first_name").disabled = state;
+    document.getElementById("second_name").disabled = state;
+    document.getElementById("bio").disabled = state;
+ }
+ 
+ //this will be updated to save information to DB
+ $(document).ready(function(){
+    $("#save_button").click(function(){
+        console.log("this will save information");
+    });
+});
+
+ $(document).ready(function(){
     $("#edit_button").click(function(){
         console.log("hide");
       $("#edit_button").hide();
+      $("#save_close_button").show();
+      toggle_readonly(false);
     });
-    $("#upload_pic").click(function(){
+    $("#close_button").click(function(){
         $("#edit_button").show();
+        $("#save_close_button").hide();
+        toggle_readonly(true);
       });
 });
-
-// function myfunction(){
-//     console.log("hide");
-//        $("#edit_button").hide();
-
-// }
-
-// $("#edit_button").click(function(){
-//     console.log("hide");
-//     $("#edit_button").toggle();
-//   });
-
-// $(document).ready(function(){
-//     $("#hide").click(function(){
-//       $("p").hide();
-//     });
-//     $("#show").click(function(){
-//       $("p").show();
-//     });
-//   });
